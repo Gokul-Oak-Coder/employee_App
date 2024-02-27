d3b7b617537c9fcb5f391832cda0b830e96e8a86
gokul <gokuloakcoder@gmail.com>
2024-02-27 13:00:16 +0530

    second

--- app/src/main/java/com/example/employeeapp/Company.java
+++ app/src/main/java/com/example/employeeapp/Company.java
@@ -10,8 +10,4 @@ public class Company {
     public String getCompanyName() {
         return companyName;
     }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
 }
--- app/src/main/java/com/example/employeeapp/Employee.java
+++ app/src/main/java/com/example/employeeapp/Employee.java
@@ -1,53 +1,43 @@
 package com.example.employeeapp;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Employee implements Serializable {
-    private int id;
+    private String id;
     private String name;
     private String email;
     private String phone;
-
+    private String website;
   //  private Company company;
 
+    // private Address address;
 
-
-    public Employee(int id, String name, String email, String phone) {
+    public Employee(String id, String name, String email, String phone, String website) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.website = website;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public String getWebsite() {
+        return website;
     }
 }
